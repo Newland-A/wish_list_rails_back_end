9c548795646b85d63e0550197e47004eac00dbe9
Newland-A <ajn252@gmail.com>
2020-12-12 17:30:04 -0500

    adjust the white list urls so that urls loocalhost:8080 has unrestricted access but all urls only have read requests.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,17 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:8080'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get]
   end
 end
