93ebced49f795110de163a9e4e418a3ced792475
Newland-A <ajn252@gmail.com>
2020-12-12 17:31:27 -0500

    reject if nested attributes are all empty

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -1,5 +1,4 @@
 class WishList < ApplicationRecord
   has_many :wish_items, dependent: :destroy
-  accepts_nested_attributes_for :wish_items, allow_destroy: true
-  #, reject_if: :all_blank
+  accepts_nested_attributes_for :wish_items, allow_destroy: true, reject_if: :all_blank
 end
