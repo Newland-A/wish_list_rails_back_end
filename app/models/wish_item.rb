ea49706cf44fc773e4cd534af35dd7755f5ac041
Newland-A <ajn252@gmail.com>
2020-12-14 06:19:17 -0500

    add validations

--- app/models/wish_item.rb
+++ app/models/wish_item.rb
@@ -1,6 +1,11 @@
 class WishItem < ApplicationRecord
   belongs_to :wish_list
+
+  validates :name, :color, :price, presence: true
+
+  validates :description, :presence => {message: "Description content cannot be blank."}, :length => { :minimum => 10, message: "Message is too short."}, :on => :create
 end
+
 # Do something like this to randomize the items
 # def randomize
 #   WishItem.all.each do |wl|
--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -1,4 +1,7 @@
 class WishList < ApplicationRecord
   has_many :wish_items, dependent: :destroy
-  accepts_nested_attributes_for :wish_items, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :wish_items, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? || attributes['color'].blank? || attributes['description'].blank?}
+  #:all_blank
+
+  validates :title, presence: true
 end
