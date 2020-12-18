47669502e3da0834803900c4a7c8054ea65a1764
Newland-A <ajn252@gmail.com>
2020-12-18 05:51:00 -0500

    remove unused code and notes

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -1,5 +1,6 @@
 class WishList < ApplicationRecord
   has_many :wish_items, dependent: :destroy
+  
   accepts_nested_attributes_for :wish_items, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? || attributes['color'].blank? || attributes['description'].blank?}
   #:all_blank
 
