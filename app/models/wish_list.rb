57908ea55db3c7d48c3e33b715ac3e62858a89be
Newland-A <ajn252@gmail.com>
2020-12-28 09:02:15 -0500

    add html for list item and appendto container

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -1,7 +1,9 @@
 class WishList < ApplicationRecord
   has_many :wish_items, dependent: :destroy
   
-  accepts_nested_attributes_for :wish_items, allow_destroy: true, reject_if: proc { |attributes| attributes['name'].blank? || attributes['color'].blank? || attributes['description'].blank?}
+  accepts_nested_attributes_for :wish_items, allow_destroy: true
+  
+  # reject_if: proc { |attributes| attributes['name'].blank? || attributes['color'].blank? || attributes['description'].blank?}
   #:all_blank
 
   validates :title, presence: true
